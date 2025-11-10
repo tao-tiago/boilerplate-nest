4c3493288b63e34798ebdc516cad3ce1cb358000
Tiago Pires <tao.pires@gmail.com>
2025-11-10 14:15:19 -0300

    fix: default parameter in constructor

--- src/core/infra/warning/index.ts
+++ src/core/infra/warning/index.ts
@@ -1,13 +1,13 @@
-import { Logger } from "../log/logger"
+import { Logger, logger } from "../log/logger"
 
 export class Warning {
   public readonly message: string[]
   public readonly status: number
   public readonly logger: Partial<Logger>
 
-  constructor(message: string | string[], status = 500, loggerError?: Partial<Logger>) {
+  constructor(message: string | string[], status = 500, loggerError: Partial<Logger> = { ...logger }) {
     this.status = status
-    this.logger = loggerError ?? { ...this.logger }
+    this.logger = loggerError
 
     if (typeof message === "string") {
       this.message = [message]
